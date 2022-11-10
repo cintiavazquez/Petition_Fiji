9c2c85ad0289484541b6fc4f8222d3b745d9ce67
Cintia Vazquez <vazquezalvarezcintia@gmail.com>
2022-11-10 11:28:22 +0100

    --refactor: export routes to their own files

---
+++ routes/signatures.js
@@ -0,0 +1,105 @@
+const express = require("express");
+const router = express.Router();
+const {
+    getSignatures,
+    createSignature,
+    getSignatureByUserID,
+    getUserById,
+    deleteSignature,
+    getSignaturesByCity,
+} = require("../db");
+const { checkLogin, checkSignature } = require("./middleware");
+
+router.post("/petition", checkLogin, (request, response) => {
+    let user_id = request.session.user_id;
+
+    const { signature } = request.body;
+
+    if (!signature.length) {
+        console.log("Error: you did not sign!");
+        const error = "error";
+        response.render("petition", {
+            error: error,
+        });
+        return;
+    }
+
+    createSignature({ user_id, signature })
+        .then((result) => {
+            console.log("created signature: ", result);
+            //🍪
+            request.session.signatureId = result.id;
+            response.redirect("/thank-you");
+        })
+        .catch((error) => {
+            console.log("error creating signature", error);
+            response.redirect("/");
+        });
+});
+
+router.get("/thank-you", checkLogin, checkSignature, (request, response) => {
+    getUserById(request.session.user_id)
+        .then((foundUser) => foundUser)
+        .then((foundUser) =>
+            getSignatureByUserID(request.session.user_id)
+                .then((result) => {
+                    response.render("thank-you", {
+                        signature: result.signature,
+                        foundUser,
+                    });
+                })
+                .catch((error) => {
+                    console.log("Error getting signature by ID", error);
+                    response.render("500");
+                })
+        )
+        .catch((error) => console.log("error retrieving user", error));
+});
+
+router.post("/thank-you", (request, response) => {
+    let user_id = request.session.user_id;
+    deleteSignature({ user_id })
+        .then(() => {
+            request.session.signatureId = null;
+            response.redirect("/petition");
+        })
+        .catch((error) =>
+            console.log("/thank-you: error deleting the signature", error)
+        );
+});
+
+router.get("/signatures", checkLogin, checkSignature, (request, response) => {
+    getSignatures()
+        .then((signees) => {
+            response.render("signatures", {
+                signees: signees,
+            });
+        })
+        .catch((error) => {
+            console.log("/signatures: error displaying signatures", error);
+            response.status(404);
+        });
+});
+
+router.get(
+    "/petition/:city",
+    checkLogin,
+    checkSignature,
+    (request, response) => {
+        let city = request.params.city;
+
+        getSignaturesByCity(city)
+            .then((signees) => {
+                response.render("cities", {
+                    signees: signees,
+                    city,
+                });
+            })
+            .catch((error) => {
+                console.log("/petition/:city: error displaying signers", error);
+                response.status(404);
+            });
+    }
+);
+
+module.exports = router;
---
+++ routes/user.js
@@ -0,0 +1,200 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+    createUser,
+    login,
+    createUserProfile,
+    getUserInfo,
+    updateUser,
+    updateUserNoPass,
+    upsertUserProfile,
+} = require("../db");
+
+const { checkLogin } = require("./middleware");
+
+router.get("/", (request, response) => {
+    response.redirect("/register");
+});
+
+router.get("/register", (request, response) => {
+    if (request.session.user_id) {
+        response.redirect("/petition");
+        return;
+    }
+    response.render("register");
+});
+
+router.post("/register", (request, response) => {
+    const { first_name, last_name, email, password } = request.body;
+
+    if (
+        !first_name.length ||
+        !last_name.length ||
+        !email.length ||
+        !password.length
+    ) {
+        console.log("Error: not all fields were filled");
+        const error = "error";
+        response.render("register", {
+            error: error,
+        });
+        return;
+    }
+
+    createUser({ first_name, last_name, email, password })
+        .then((result) => {
+            //🍪
+            request.session.user_id = result.id;
+            response.redirect("/profile");
+        })
+        .catch((error) => {
+            console.log("error creating user: ", error);
+            if (error.constraint === "users_email_key") {
+                response
+                    .status(409)
+                    .render("register", { duplicate: error.constraint });
+            }
+        });
+});
+
+router.get("/profile", checkLogin, (request, response) => {
+    response.render("profile");
+});
+
+router.post("/profile", checkLogin, (request, response) => {
+    let user_id = request.session.user_id;
+    let { age, city, homepage } = request.body;
+
+    createUserProfile({ user_id, age, city, homepage })
+        .then(() => {
+            response.redirect("/petition");
+        })
+        .catch((error) => {
+            console.log("error creating user: ", error);
+            response.status(500).render("/profile", {
+                error: error,
+            });
+        });
+});
+
+router.get("/profile/edit", checkLogin, (request, response) => {
+    getUserInfo({ user_id: request.session.user_id })
+        .then((userInfo) => {
+            response.render("profileEdit", userInfo);
+        })
+        .catch((error) => {
+            console.log("/profile/edit: error retrieving user info", error);
+            response.render("500");
+        });
+});
+
+router.post("/profile/edit", checkLogin, (request, response) => {
+    let user_id = request.session.user_id;
+    let { first_name, last_name, email, password, age, city, homepage } =
+        request.body;
+
+    age = parseInt(age);
+
+    upsertUserProfile({ user_id, age, city, homepage })
+        .then((result) => {
+            console.log(result);
+        })
+        .catch((error) =>
+            console.log(
+                "/profile/edit: could not update user in upsertUserProfile",
+                error
+            )
+        );
+
+    if (password === "") {
+        updateUserNoPass({
+            first_name,
+            last_name,
+            email,
+            user_id: request.session.user_id,
+        })
+            .then(() => {
+                response.redirect("/petition");
+            })
+            .catch((error) =>
+                console.log(
+                    "/profile/edit: could not update user in updateUserNoPass ",
+                    error
+                )
+            );
+        return;
+    }
+    updateUser({
+        first_name,
+        last_name,
+        email,
+        password,
+        user_id: request.session.user_id,
+    })
+        .then(() => {
+            response.redirect("/petition");
+        })
+        .catch((error) => console.log("could not update user", error));
+});
+
+router.post("/login", (request, response) => {
+    const { email, password } = request.body;
+
+    if (!email.length || !password.length) {
+        const error = "error";
+        response.render("login", {
+            error: error,
+        });
+        return;
+    }
+
+    login({
+        email,
+        password,
+    })
+        .then((foundUser) => {
+            request.session.user_id = foundUser.user_id;
+            request.session.signatureId = !!foundUser.signature;
+            response.redirect("/petition");
+        })
+        .catch((error) => {
+            console.log("error logging in", error);
+            response.render("login", {
+                error: error,
+            });
+        });
+});
+
+router.get("/login", (request, response) => {
+    if (request.session.user_id) {
+        response.redirect("/petition");
+        return;
+    }
+    response.render("login");
+});
+
+router.get("/petition", checkLogin, (request, response) => {
+    if (request.session.signatureId) {
+        response.redirect("/thank-you");
+        return;
+    }
+    getUserInfo({ user_id: request.session.user_id })
+        .then(() => {
+            response.render("petition");
+        })
+        .catch((error) => {
+            createUserProfile({ user_id: request.session.user_id })
+                .then(() => response.render("petition"))
+                .catch(
+                    (error) => ("/petition: error creating user profile", error)
+                );
+        });
+});
+
+router.post("/logout", (request, response) => {
+    request.session = null;
+    response.redirect("/");
+});
+
+module.exports = router;
